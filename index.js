ae15de51f6472c28a9bf9e8fca3db5235c447ad1
martii0105 <128637734+martii0105@users.noreply.github.com>
2023-05-04 19:53:07 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('CharliexNati\nBot\nMD', {
+say('Martiibot\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
